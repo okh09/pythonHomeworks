b53bbd1e80b44c6c4fca30179947fd6886a1a6fc
Oybek Khamidov <98575988+okh09@users.noreply.github.com>
2022-10-31 23:42:05 +0500

    homework_sem4

---
+++ sem4/sem4task1.py
@@ -0,0 +1,30 @@
+'''задача 1. Задайте натуральное число N. Напишите программу,
+которая составит список простых множителей числа N.'''
+
+import math
+
+
+def is_prime(n):
+    primes = [2]
+    for num in range(3, n + 1, 2):
+        if all(num % i != 0 for i in range(2, int(math.sqrt(num)) + 1)):
+            primes.append(num)
+    return primes
+
+
+def get_prime_factors(n, primes):
+    fact = []
+    for i in primes:
+        while n % i == 0:
+            n = n / i
+            fact.append(i)
+    return fact
+
+
+n = int(input('Введите число: '))
+
+primes = is_prime(n)
+factors = get_prime_factors(n, primes)
+print(factors)
+
+
